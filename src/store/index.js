b779ed16fcf55753f1853dfe3361bc3748b9fde4
mtsdalmolin <mtsdalmolin@gmail.com>
2020-03-06 14:53:47 -0300

    Importação do axios e da variável API_URL no store/index.js

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
 
 Vue.use(Vuex)
+
+import { API_URL } from '@/assets/variables'
 
 export default new Vuex.Store({
   state: {
