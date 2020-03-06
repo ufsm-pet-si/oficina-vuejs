b151c1b0ea5922fa01f59f635328747e6075c86f
mtsdalmolin <mtsdalmolin@gmail.com>
2020-03-06 14:44:17 -0300

    Formulário de Login, com interligações dos inputs utilizando a diretiva v-model (Two-Way Binding)

---
+++ src/views/Login.vue
@@ -0,0 +1,38 @@
+<template>
+<div class="container">
+  <div class="content">
+    <p>Type your e-mail and password to Sign In!</p>
+
+    <form>
+      <label for="email">E-mail</label>
+      <input 
+        v-model="email" 
+        type="email" 
+        name="email" 
+        placeholder="Digite o seu e-mail"
+        >
+      
+      <label for="password">Senha</label>
+      <input 
+        v-model="password" 
+        type="password" 
+        name="password" 
+        placeholder="Digite a sua senha"
+        >
+
+      <button type="submit">Sign In!</button>
+    </form>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      email: '',
+      password: ''
+    }
+  }
+}
+</script>
