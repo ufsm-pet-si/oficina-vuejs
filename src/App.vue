a931202dcf6ca29c937db45779eeb4834e84ae50
mtsdalmolin <mtsdalmolin@gmail.com>
2020-03-06 17:50:58 -0300

    Adicionada uma funcionalidade extra, para o usuário logado ver uma lista personalizada apenas com os posts de sua autoria. É utilizado o atributo especial :key, pois o parâmetro userId é enviado como um query parameter. Isso faz com que o elemento seja renderizado novamente caso o valor desse atributo mude, e se observamos o valor que está no :key do router-view, é exatamente o fullPath da nossa URL. O VueJS apenas atualiza a rota caso mude um parâmetro, ignorando os parâmetros query.

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,11 @@
     <div id="nav" v-if="isLoggedIn">
       <router-link to="/">Home</router-link> |
       <router-link to="/posts">Posts</router-link> |
+      <router-link :to="`/posts?ownerId=${loggedUser.id}`">My Posts</router-link>
       <span>Olá, <router-link :to="`/users/${loggedUser.id}/edit`">{{ loggedUser.username }}</router-link></span>
       <button @click="logout">Logout</button>
     </div>
-    <router-view/>
+    <router-view :key="$route.fullPath"/>
   </div>
 </template>
 
